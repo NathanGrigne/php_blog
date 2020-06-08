3e3d4e97e3ca818ea13af16cfcf0d5236add6c21
nathangrigne <nathan.grigne@hetic.net>
2020-06-08 23:26:15 +0200

    update account style and home page

--- account.php
+++ account.php
@@ -5,11 +5,14 @@
     $query = $pdo->query('SELECT text, id_comment FROM comments WHERE username ="'.$_SESSION['username'].'"');
     $comments = $query->fetchAll();
 
-    include './includes/header.php';
+    include 'includes/header.php';
 ?>
 
 <h1><?= $_SESSION['username'] ?></h1>
 
+<h2>Tous ses commentaires :</h2>
 <?php foreach($comments as $_comments): ?>
-    <p><?= $_comments->text ?></p>
+    <div class="comments">
+        <p><?= $_comments->text ?></p>
+    </div>
 <?php endforeach ?>
